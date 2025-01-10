651fb021e3d1f9eb5468224bf6a13ab26d8b1def
Cathleen Vu <cathleendvu@gmail.com>
2025-01-10 14:57:01 -0800

    Implement game logic with nextMove and nextRound functions, add new states, and fix alert behavior

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -11,9 +11,12 @@ struct ContentView: View {
     var moves = ["Rock", "Paper", "Scissor"]
     @State var roundsPlayed = 0
     @State var playerScore = 0
-    @State var playerMove: String = ""
+    @State var playerMove = ""
     @State var appChoice = Int.random(in: 0...2)
     @State var shouldWin = Bool.random()
+    @State var roundTitle = ""
+    @State var showAlert = false
+    @State var isGameOver = false
     
     var body: some View {
         VStack {
@@ -33,15 +36,64 @@ struct ContentView: View {
         
         HStack {
             ForEach(0...2, id: \.self) { number in
-                Button(action: nextMove) {
+                Button(action: {nextMove(number)}) {  // Curly braces are required for action when you pass in a parameter to the action
                     Text(moves[number])
                 }
             }
         }
+        .alert(roundTitle, isPresented: $showAlert) {
+            Button(action: nextRound) {
+                Text("Continue")
+            }
+        } message: {
+            Text("Your score is \(playerScore)/6")
+        }
     }
     
-    func nextMove() {
+    func nextMove(_ playerChoice: Int) {
+        playerMove = moves[playerChoice]
+        let appMove = moves[appChoice]
         
+        if shouldWin {
+            if (playerMove == "Paper" && appMove == "Rock") || (playerMove == "Scissor" && appMove == "Paper") || (playerMove == "Rock" && appMove == "Scissor") {
+                roundTitle = "Win!"
+                playerScore += 1
+            }
+            else {
+                roundTitle = "Lose!"
+            }
+        } else {
+            if (playerMove == "Scissor" && appMove == "Rock") || (playerMove == "Rock" && appMove == "Paper") || (playerMove == "Paper" && appMove == "Scissor") {
+                roundTitle = "Win!"
+                playerScore += 1
+            }
+            else {
+                roundTitle = "Lose!"
+            }
+        }
+        
+        // Check if game is over
+        if roundsPlayed == 5 {
+            isGameOver = true
+            roundTitle = playerScore < 6 ? "Game Over! You lost the game." : "Congratulations! You won the game!"
+        } else {
+            isGameOver = false
+        }
+        
+        roundsPlayed += 1
+        showAlert = true
+    }
+    
+    func nextRound() {
+        if (roundsPlayed == 6) {
+            // Reset the game
+            playerScore = 0
+            roundsPlayed = 0
+        }
+        
+        showAlert = false
+        appChoice = Int.random(in: 0...2)
+        shouldWin.toggle()  // Toggle shouldWin after each round
     }
 }
 
