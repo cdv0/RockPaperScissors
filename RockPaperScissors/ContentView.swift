31b218eec0e0b472a4dcc65c027fb6a0bfcd3255
Cathleen Vu <cathleendvu@gmail.com>
2025-01-10 13:58:33 -0800

    Create initial structure for the Rock Paper Scissors game UI

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    var moves = ["Rock", "Paper", "Scissors"]
+    @State var playerScore = 0
+    @State var playerMove: String = ""
+    @State var appMove = Int.random(in: 0...2)
+    @State var shouldWin = Bool.random()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Rock Paper Scissors")
+                .font(.largeTitle)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            Text("Player Score: \(playerScore)/6")
+                .font(.system(size: 20))
         }
-        .padding()
+        VStack {
+            Text("App Move: \(moves[appMove])")
+            Text("Player should: \(shouldWin ? "Win" : "Lose")")
+        }
+        .font(.system(size: 20))
+        
+        HStack {
+            ForEach(0...2, id: \.self) { number in
+                Button(action: nextMove) {
+                    Text(moves[number])
+                }
+            }
+        }
+    }
+    
+    func nextMove() {
+        
     }
 }
 
