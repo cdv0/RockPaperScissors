5cc4b331c3f95f3a62b9caa3c8a6e1c5d3e472e3
Cathleen Vu <cathleendvu@gmail.com>
2025-01-09 19:49:55 -0800

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Cathleen Vu on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
